e7d911d3b34f52f7cba2c6317e8244823fc19acb
kaisa.ets <kaisaets@gmail.com>
2025-11-22 11:54:43 +0200

    edit function, enabled column

--- src/main/java/com/company/webapp/user/UserController.java
+++ src/main/java/com/company/webapp/user/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -37,4 +38,15 @@ public class UserController {
         return "redirect:/users";
     }
 
+    @GetMapping("/users/edit/{id}")
+    public String showEditForm(@PathVariable("id") int id, Model model, RedirectAttributes ra){
+        try {
+            User user = service.get(id);
+            model.addAttribute("user", user);
+            model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
+            return "user_form";
+        } catch (UserNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
   }
--- src/main/java/com/company/webapp/user/UserService.java
+++ src/main/java/com/company/webapp/user/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 
@@ -16,5 +17,13 @@ public class UserService {
 
     public void save(User user) {
         repo.save(user);
+    }
+
+    public User get(Integer id) throws UserNotFoundException {
+        Optional<User> result = repo.findById(id);
+        if (result.isPresent()) {
+            return result.get();
+        }
+        throw new UserNotFoundException("Could not find user with this ID");
     }
 }
